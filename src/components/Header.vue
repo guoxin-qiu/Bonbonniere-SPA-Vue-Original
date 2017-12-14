7e48be60478a379d072f87a6d414a2ad1cc058dc
Denis <guoxin.qiu@outlook.com>
2017-12-14 10:04:26 +0800

    aviod refresh

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,24 +2,26 @@
 <div id="app-header" name="app-header">
   <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
-      <div class="navbar-header"><a class="navbar-brand">Bonbonniere</a></div>
-        <div class="navbar-collapse collapse" id="navigation-menu">
-          <ul class="nav navbar-nav">
-            <li v-for="(menu,index) in menus" :key="index">
-              <a :href="menu.Url" v-text="menu.Text"></a>
-            </li>
+      <div class="navbar-header">
+        <router-link to="/" class="navbar-brand">Bonbonniere</router-link>
+      </div>
+      <div class="navbar-collapse collapse" id="navigation-menu">
+        <ul class="nav navbar-nav">
+          <li v-for="(menu,index) in menus" :key="index">
+            <router-link :to="menu.Url" v-text="menu.Text"></router-link>
+          </li>
+        </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a v-text="fullName"></a></li>
+            <li><button @click="logoff" class="btn btn-link navbar-btn navbar-link">Logoff</button></li>
           </ul>
-            <ul class="nav navbar-nav navbar-right">
-              <li><a v-text="fullName"></a></li>
-              <li><button @click="logoff" class="btn btn-link navbar-btn navbar-link">Logoff</button></li>
-            </ul>
-        </div>
+      </div>
     </div>
   </nav>
 </div>
 </template>
 <style>
-.navbar-inverse{
+.navbar-inverse {
   background-color: white;
 }
 </style>
@@ -35,14 +37,14 @@ export default {
   },
   methods: {
     logoff() {
-      api.logoff()
-      this.$router.push({ path: "/login" })
-    },
+      api.logoff();
+      this.$router.push({ path: "/login" });
+    }
   },
   beforeCreate() {
     let _self = this;
     api.getMenu().then(function(data) {
-      _self.$data.menus = data
+      _self.$data.menus = data;
     });
   }
 };
