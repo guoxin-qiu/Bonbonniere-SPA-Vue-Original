61a9f2cac6ee5a91c5f10a18bde0f954a7c2895b
Denis <guoxin.qiu@outlook.com>
2017-12-14 14:13:04 +0800

    change folder name from pages to views

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Login from '@/pages/Login.vue'
+import Login from '@/views/Login.vue'
 
 Vue.use(Router)
 
@@ -21,7 +21,7 @@ export default new Router({
     {
       path: '/home',
       name: 'Home',
-      component: resolve => require(['../pages/Home.vue'], resolve),
+      component: resolve => require(['../views/Home.vue'], resolve),
       meta: { requireAuth: true }
     }
   ]
