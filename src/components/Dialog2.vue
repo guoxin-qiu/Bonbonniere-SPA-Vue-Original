8f992b6e7bb96b99a8b2ac893553026abc588e3e
Denis <guoxin.qiu@outlook.com>
2018-02-08 19:20:31 +0800

    add new dialog

---
+++ src/components/Dialog2.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="modalDialog">
+    <div>
+      <a title="Close" class="close" @click="$emit('close')">X</a>
+      <div class="dialog-content">
+        <div class="dialog-header">
+          <slot name="dialog-header"></slot>
+        </div>
+        <div class="dialog-body">
+          <slot name="dialog-body"></slot>
+        </div>
+        <div class="dialog-footer">
+          <slot name="dialog-footer"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.modalDialog {
+  position: fixed;
+  font-family: Arial, Helvetica, sans-serif;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.4);
+  z-index: 999;
+  opacity: 0;
+  -webkit-transition: opacity 400ms ease-in;
+  -moz-transition: opacity 400ms ease-in;
+  transition: opacity 400ms ease-in;
+  pointer-events: none;
+  opacity: 1;
+  pointer-events: auto;
+}
+
+.modalDialog:target {
+  opacity: 1;
+  pointer-events: auto;
+}
+
+.modalDialog > div {
+  width: 400px;
+  position: relative;
+  margin: 10% auto;
+  padding: 5px 20px 13px 20px;
+  border-radius: 10px;
+  background: #fff;
+  background: -moz-linear-gradient(#fff, #999);
+  background: -webkit-linear-gradient(#fff, #999);
+  background: -o-linear-gradient(#fff, #999);
+}
+
+.close {
+  background: #606061;
+  color: #ffffff;
+  line-height: 25px;
+  position: absolute;
+  right: -12px;
+  text-align: center;
+  top: -10px;
+  width: 24px;
+  text-decoration: none;
+  font-weight: bold;
+  -webkit-border-radius: 12px;
+  -moz-border-radius: 12px;
+  border-radius: 12px;
+  -moz-box-shadow: 1px 1px 3px #000;
+  -webkit-box-shadow: 1px 1px 3px #000;
+  box-shadow: 1px 1px 3px #000;
+}
+
+.close:hover {
+  background: #00d9ff;
+}
+/* .dialog-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.dialog-header .close span {
+  font-size: 36px;
+}
+
+.dialog-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.dialog-enter {
+  opacity: 0;
+}
+
+.dialog-leave-active {
+  opacity: 0;
+}
+
+.dialog-enter .dialog-content,
+.dialog-leave-active .dialog-content {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+
+.dialog-content {
+  width: 800px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+} */
+</style>
